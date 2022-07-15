02affb7215c85bbd8f3a1c374ec0dfa091052e4b
rutiksimform <rutik.p@simformsolutions.com>
2022-07-15 15:48:47 +0530

    permit parameters in subjects.rb

--- app/admin/subjects.rb
+++ app/admin/subjects.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Subject do
-
+  permit_params :name, note_ids: []
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Subject do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
